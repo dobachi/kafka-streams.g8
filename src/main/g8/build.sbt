cfb67a094f7712762ca93042fb9a334db99521da
dobachi <dobachi1983oss@gmail.com>
2020-12-11 22:53:02 +0900

    Update versions of libraries

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -12,17 +12,27 @@ lazy val root = (project in file(".")).settings(
     library.log4jCore,
     library.typesafeConfig,
     library.kafkaTest,
+    library.scalactic,
     library.scalaTest
   )
+).settings(
+  assemblyMergeStrategy in assembly := {
+    case PathList("jackson-databind-2.10.2.jar") => MergeStrategy.last
+    case PathList("jackson-annotations-2.10.2.jar") => MergeStrategy.last
+    case PathList("jackson-core-2.10.2.jar") => MergeStrategy.last
+    case PathList("jackson-datatype-jdk8-2.10.2.jar") => MergeStrategy.last
+    case PathList("META-INF", "MANIFEST.MF") => MergeStrategy.discard
+    case _ => MergeStrategy.first
+  }
 )
 
 lazy val library = new {
 
   val version = new {
     val kafkaVersion   = "2.6.0"
-    val scalaTest      = "3.0.5"
-    val log4jCore      = "2.11.1"
-    val typesafeConfig = "1.3.2"
+    val scalaTest      = "3.2.3"
+    val log4jCore      = "2.13.3"
+    val typesafeConfig = "1.3.4"
   }
 
   val kafkaClients   = "org.apache.kafka"         % "kafka-clients"        % version.kafkaVersion
@@ -31,5 +41,6 @@ lazy val library = new {
   val typesafeConfig = "com.typesafe"             % "config"               % version.typesafeConfig
 
   val kafkaTest = "org.apache.kafka" % "kafka-streams-test-utils" % version.kafkaVersion
+  val scalactic = "org.scalactic"    %% "scalactic"               % version.scalaTest
   val scalaTest = "org.scalatest"    %% "scalatest"               % version.scalaTest % "test"
 }
--- src/main/g8/src/test/scala/$package__packaged$/WordCountSpec.scala
+++ src/main/g8/src/test/scala/$package__packaged$/WordCountSpec.scala
@@ -1,26 +1,29 @@
 package $package$
 
-import java.lang.Long
 import java.util.Properties
 
+import java.lang.Long
 import org.apache.kafka.common.serialization._
 import org.apache.kafka.streams.scala.Serdes
 import org.apache.kafka.streams.test.{ ConsumerRecordFactory, OutputVerifier }
 import org.apache.kafka.streams.{ StreamsConfig, Topology, TopologyTestDriver }
-import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpec }
+import org.scalatest._
+import matchers._
+import wordspec._
 
-class WordCountSpec extends WordSpec with Matchers with BeforeAndAfterAll {
+
+class WordCountSpec extends AnyWordSpec with should.Matchers with BeforeAndAfterAll {
 
   private val stringDeserializer = Serdes.String.deserializer()
-  private val longDeserializer   = new LongDeserializer
+  private val longDeserializer: Deserializer[java.lang.Long]   = new LongDeserializer()
 
   val props = new Properties()
   props.put(StreamsConfig.APPLICATION_ID_CONFIG, StreamSettings.appID)
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, StreamSettings.bootstrapServers)
 
   val recordFactory = new ConsumerRecordFactory[String, String](StreamSettings.inputTopic,
-                                                                new StringSerializer,
-                                                                new StringSerializer)
+    new StringSerializer,
+    new StringSerializer)
 
   val topology: Topology             = WordCount.topology().build()
   val testDriver: TopologyTestDriver = new TopologyTestDriver(topology, props)
@@ -28,15 +31,15 @@ class WordCountSpec extends WordSpec with Matchers with BeforeAndAfterAll {
   "WordCount" should {
     "count words correctly" in {
       testDriver.pipeInput(recordFactory.create(StreamSettings.inputTopic, "", "kafka kafka"))
-      OutputVerifier.compareKeyValue(
+      OutputVerifier.compareKeyValue[String, java.lang.Long](
         testDriver.readOutput(StreamSettings.outputTopic, stringDeserializer, longDeserializer),
         "kafka",
-        1L: Long
+        1L
       )
-      OutputVerifier.compareKeyValue(
+      OutputVerifier.compareKeyValue[String, java.lang.Long](
         testDriver.readOutput(StreamSettings.outputTopic, stringDeserializer, longDeserializer),
         "kafka",
-        2L: Long
+        2L
       )
     }
   }
